4d94beaa1f006d88a97b169c8aa9ced1bdd7edde
chupexy <osaretin520@gmail.com>
2024-08-09 14:45:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
+const mongoose = require('mongoose')
+
+
+const app = express()
+
+
+
+
+// connect database
+mongoose.connect(process.env.MONGO_URI)
+const con = mongoose.connection
+con.on('open', error =>{
+    if(error) {
+        console.log(`Error connecting to database ${error}`)
+    }else{
+    console.log("Connected to Database")
+    }
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//routes
+
+
+
+const port = process.env.PORT
+app.listen(port , ()=>{
+    console.log(`server listening at port ${port}`);
+})
+
+module.exports = app
